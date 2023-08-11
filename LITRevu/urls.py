3361e54079feb21f25c78ab9c47f86a5c5ed1557
Thomas <thomas.savelli92@gmail.com>
2023-08-11 16:33:02 +0200

    construction de l'app authentication

--- LITRevu/urls.py
+++ LITRevu/urls.py
@@ -15,8 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import (LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
 from django.urls import path
+
+import authentication.views
+import reviews_app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LoginView.as_view(
+        template_name='authentication/login.html',
+        redirect_authenticated_user=True),
+        name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('change-password/', PasswordChangeView.as_view(
+        template_name = 'authentication/password_change_form.html'),
+        name= 'password_change'),
+    path('change_password_done/', PasswordChangeDoneView.as_view(
+        template_name = 'authentication/password_change_done.html'),
+        name = 'password_change_done'),
+    path('home/', reviews_app.views.home, name='home'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
+from . import forms
 # Create your views here.
+
+class LoginPage(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        message = ''
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password =form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                message = 'Identifiants invalides'
+        return render(request, self.template_name, context={'form': form, 'message': message})
+
